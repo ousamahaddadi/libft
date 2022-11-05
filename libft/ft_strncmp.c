a5b76ba9cde2c38a9e1dfbd84cadab9f2e923b89
ohaddadi <ousama.haddadi@gmail.com>
2022-11-05 16:01:30 +0100

    commit05112022

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -1,31 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ohaddadi <ousama.haddadi@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/05 15:24:31 by ohaddadi          #+#    #+#             */
-/*   Updated: 2022/11/05 16:00:31 by ohaddadi         ###   ########.fr       */
+/*   Created: 2022/11/05 16:00:56 by ohaddadi          #+#    #+#             */
+/*   Updated: 2022/11/05 16:00:57 by ohaddadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-char	*ft_strrchr(const char *s, int c)
-{
-	unsigned int slen;
-
-	slen = (ft_strlen(s));
-	if (*(s) == (char) c)
-		return ((char *)s);
-	while (slen > 0)
-	{
-		if (*(s + slen) == (char )c)
-			return ((char *)(s + slen));
-		slen--;
-	}
-	if (c == '\0')
-		return ((char *)(s +slen));
-	return (NULL);
-}
